ece434f851e8742d18dda5d69496261cdde93fec
Haruto Mori <baystarsman@outlook.jp>
2023-03-09 13:46:43 +0100

    pass simple data into the home and display it

--- base/views.py
+++ base/views.py
@@ -2,8 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
+rooms = [
+    {'id': 1, 'name': 'Lets learn python!'},
+    {'id': 2, 'name': 'Lets learn java!'},
+    {'id': 3, 'name': 'Lets learn c!'},
+]
+
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 def room(request):
     return render(request, 'room.html')
