d4c4647fe9fdc5166d035c17b274b160c15b1967
Haruto Mori <baystarsman@outlook.jp>
2023-03-11 01:37:39 +0100

    create a edit and update room function

--- base/views.py
+++ base/views.py
@@ -31,3 +31,17 @@ def create_room(request):
 
     context = {'form': form}
     return render(request, 'base/room_form.html', context)
+
+def update_room(request, pk):
+    room = Room.objects.get(id=pk)
+    form = RoomForm(instance=room)  # pre-filled room info when editing it
+
+    if request.method == 'POST':
+        form = RoomForm(request.POST, instance=room)
+        
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
