53d8f934f44ac880279cb72a3cea747be00a110a
Haruto Mori <baystarsman@outlook.jp>
2023-03-17 11:40:07 +0100

    build a function for user account page taht can edit profile

--- base/forms.py
+++ base/forms.py
@@ -5,9 +5,15 @@
 
 from django.forms import ModelForm
 from .models import Room
+from django.contrib.auth.models import User
 
 class RoomForm(ModelForm):
     class Meta:
         model = Room
         fields = '__all__'
         exclude = ['host', 'participants']  # dont display host and participants section in the create roon form
+
+class UserForm(ModelForm):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
