2da83bba09c2eac175405b26d7a97e1bc1b057e1
Haruto Mori <baystarsman@outlook.jp>
2023-03-21 15:58:56 +0100

    build a api functions to get app routes and rooms info

--- discordlike/settings.py
+++ discordlike/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'base.apps.BaseConfig',
+
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
--- discordlike/urls.py
+++ discordlike/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', include('base.urls')),
+    path('api/', include('base.api.urls')),
 ]
